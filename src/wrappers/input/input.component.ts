1b8a69bd61bca438881ebf2269e9f3f26fd868c3
hippogrammer <hippogrammer@gmail.com>
2020-06-20 12:43:56 -0400

    removed unneeded code

--- src/wrappers/input/input.component.ts
+++ src/wrappers/input/input.component.ts
@@ -60,12 +60,6 @@ export class InputComponent implements OnInit, OnChanges, AfterViewChecked {
         return `min ${this.control.errors.minlength.requiredLength} digits`;
       }
       case this.control.hasError('maxlength'): {
-        let maxLen = this.control.errors.maxlength.requiredLength; 
-        if(this.type === INPUT_TYPES.tel) {
-          if(this.control.value.replace(/[\(\)\-]/g, '').length > maxLen){
-            return `max ${this.control.errors.maxlength.requiredLength} digits`;
-          }else { break } 
-        }
         return `max ${this.control.errors.maxlength.requiredLength} digits`;
       }
       default: {
